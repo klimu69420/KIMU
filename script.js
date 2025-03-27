8cbc7af298a5ad224308de89fd6a3ce4bedbe78a
klimu69420 <gdam385@gmail.com>
2025-03-27 21:29:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth Scroll Effect
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
